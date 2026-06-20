65ac50ef65acb915b7b3e54e195e3f4055f6a75f
Mr-IKE01 <shahbazabutt77@gmail.com>
2026-06-20 23:57:20 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-zinc-950 antialiased">{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   )
 }
